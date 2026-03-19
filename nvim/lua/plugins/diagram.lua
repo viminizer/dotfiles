89314a14a4d221457f03d52fd02847db0fff73f1
SHOKHRUKH TURSUNOV <str9703@gmail.com>
2026-03-19 09:32:22 +0900

    feat: add diagram.lua to nvim

---
+++ nvim/lua/plugins/diagram.lua
@@ -0,0 +1,45 @@
+-- Setup:
+-- brew install imagemagick
+-- npm install -g @mermaid-js/mermaid-cli
+
+return {
+  {
+    "3rd/image.nvim",
+    build = false,
+    opts = {
+      backend = "kitty",
+      processor = "magick_cli",
+      tmux_show_only_in_active_window = true,
+      max_height_window_percentage = 100,
+      scale_factor = 2.0,
+    },
+  },
+  {
+    "3rd/diagram.nvim",
+    dependencies = { "3rd/image.nvim" },
+    ft = { "markdown" },
+    opts = {
+      events = {
+        render_buffer = {},
+        clear_buffer = { "BufLeave" },
+      },
+      renderer_options = {
+        mermaid = {
+          scale = 10,
+          theme = "dark",
+          background = "transparent",
+        },
+      },
+    },
+    keys = {
+      {
+        "<leader>cd",
+        function()
+          require("diagram").show_diagram_hover()
+        end,
+        desc = "Show diagram",
+        ft = { "markdown" },
+      },
+    },
+  },
+}
